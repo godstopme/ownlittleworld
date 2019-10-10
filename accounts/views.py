7c6430f720c1971169e5e1fbbad734212e2e998b
Nica <33498296+godstopme@users.noreply.github.com>
2019-10-10 17:04:48 +0300

    '''clearbit and emailhunter integration'''

--- accounts/views.py
+++ accounts/views.py
@@ -8,3 +8,9 @@ class SignupUserViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     permission_classes = [permissions.AllowAny]
     queryset = get_user_model().objects
     serializer_class = UserSignupSerializer
+
+    # emailhunter restricts me to signup using my regular email, also i don't like to use OAuth
+    # so logic to validate email can be placed either here in controller or directly in serializer' validation method
+    # same restrictions apply for clearbit, but additional user data upload can be done via
+    # another service in background (like celery, etc) or via django signals
+    # no need to block user signup here, profile info can be loaded later
